5b4420657ed995950e72253069cb80f0f1a32caf
felix <lambertfelix8@gmail.com>
2017-09-27 23:23:24 +0200

    second commit

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const fetch = require('node-fetch');
 
 const getAllDomains = (callback) => {
-    
     let results = [];
 
-    const getMoreDomains = (callback, after) => {
+    async function getMoreDomains(callback, after) {
         const url = 'https://www.reddit.com/r/programming.json?' + (after ? 'after=' + encodeURIComponent(after) : '');
-        fetch(url)
-        .then(res => res.json())
-        .then(res => {
-            console.log('Please wait...');
-            const domain = res.data.children.map(data => 
-                data.data.created < 1504994400 ? callback(null, results) : data.data.domain);
+        const body = await fetch(url).then(res => res.json());
+        const domain = body.data.children.map(data => 
+            data.data.created < 1504994400 ? callback(null, results) : data.data.domain);
+        
+        console.log('Please wait...');
+        const onlyUnique = (value, index, self) => self.indexOf(value) === index;
 
-            const onlyUnique = (value, index, self) => self.indexOf(value) === index;
-
-            results = results.concat(domain);
-            results = results.filter(onlyUnique);
-            if (res.data.after) {   
-                getMoreDomains(callback, res.data.after);
-            } else {
-                callback(`Can't get all the domains`, results);
-            }
-        }); 
+        results = results.concat(domain);
+        results = results.filter(onlyUnique);
+        if (body.data.after) {   
+            getMoreDomains(callback, body.data.after);
+        } else {
+            callback(`Can't get all the domains`, results);
+        }
     }
     getMoreDomains(callback);
 }
@@ -31,5 +27,3 @@ getAllDomains((err, domains) => {
     if (err) console.log('ERROR:', err);
     console.log(JSON.stringify(domains, null, 2));
 });
-
-
